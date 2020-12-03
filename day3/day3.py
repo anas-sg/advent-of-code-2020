874d7ac3631fe1226783cbff5c0171e50f522596
Muhammed Anas <anas.m6@gmail.com>
2020-12-03 15:50:05 +0800

    Initial commit

--- day3/day3.py
+++ day3/day3.py
@@ -1,19 +1,21 @@
 with open("input.txt") as f:
-    puzzle = [list(line[:-1]) for line in f]
+    puzzle = [list(line[:-1]) for line in f]  #exclude \n
 
-# row = column = 0
-# rows = len(puzzle)
-# columns = len(puzzle[0])
-# count = 0
-# while row < rows:
-#     row, column = row + 1, column + 3
-#     try:
-#         if puzzle[row][column % columns] == '#':
-#             count += 1
-#     except:
-#         break
-# print(count)
+#Part 1
+row = column = 0
+rows = len(puzzle)
+columns = len(puzzle[0])
+count = 0
+while row < rows:
+    row, column = row + 1, column + 3
+    try:
+        if puzzle[row][column % columns] == '#':
+            count += 1
+    except:
+        break
+print(count)
 
+#Part 2
 rows = len(puzzle)
 columns = len(puzzle[0])
 product = 1
@@ -27,5 +29,4 @@ for i, j in [(1,1), (3,1), (5,1), (7,1), (1,2)]:
         except:
             break
     product *= count
-
 print(product)
